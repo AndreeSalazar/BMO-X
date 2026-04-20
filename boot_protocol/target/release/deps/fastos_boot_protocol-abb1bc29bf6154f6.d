7d868ea30c9bdbca249feb21b375afacba1ed04c
eddi.salazar.dev <eddi.salazar.dev@gmail.com>
2026-04-20 14:05:52 -0500

    cambios....., 2

---
+++ boot_protocol/target/release/deps/fastos_boot_protocol-abb1bc29bf6154f6.d
@@ -0,0 +1,7 @@
+C:\Users\andre\OneDrive\Documentos\FastOS\boot_protocol\target\release\deps\fastos_boot_protocol-abb1bc29bf6154f6.d: src\lib.rs
+
+C:\Users\andre\OneDrive\Documentos\FastOS\boot_protocol\target\release\deps\libfastos_boot_protocol-abb1bc29bf6154f6.rlib: src\lib.rs
+
+C:\Users\andre\OneDrive\Documentos\FastOS\boot_protocol\target\release\deps\libfastos_boot_protocol-abb1bc29bf6154f6.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ boot_protocol/target/release/libfastos_boot_protocol.d
@@ -0,0 +1,1 @@
+C:\Users\andre\OneDrive\Documentos\FastOS\boot_protocol\target\release\libfastos_boot_protocol.rlib: C:\Users\andre\OneDrive\Documentos\FastOS\boot_protocol\src\lib.rs
